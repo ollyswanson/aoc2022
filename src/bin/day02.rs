1223725da1ed7e13afa99fd5e3a724a9cbaa36eb
Olly Swanson <olly.swanson95@gmail.com>
2022-12-02 15:23:31 +0000

    Switch from `.lines()` to `as_bytes().chunks(4)`

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -2,11 +2,11 @@ fn main() -> anyhow::Result<()> {
     let input = include_str!("../../inputs/day02.txt");
 
     let score: i32 = input
-        .lines()
-        .map(|line| {
-            let bytes = line.as_bytes();
-            let them = (bytes[0] - b'A') as i32;
-            let me = (bytes[2] - b'X') as i32;
+        .as_bytes()
+        .chunks(4)
+        .map(|chunk| {
+            let them = (chunk[0] - b'A') as i32;
+            let me = (chunk[2] - b'X') as i32;
 
             let outcome = ((me - them).rem_euclid(3) + 1) % 3;
 
@@ -17,11 +17,11 @@ fn main() -> anyhow::Result<()> {
     println!("Part 1: {}", score);
 
     let score: i32 = input
-        .lines()
-        .map(|line| {
-            let bytes = line.as_bytes();
-            let them = (bytes[0] - b'A') as i32;
-            let outcome = (bytes[2] - b'X') as i32;
+        .as_bytes()
+        .chunks(4)
+        .map(|chunk| {
+            let them = (chunk[0] - b'A') as i32;
+            let outcome = (chunk[2] - b'X') as i32;
 
             let me = (them + outcome + 2) % 3;
 

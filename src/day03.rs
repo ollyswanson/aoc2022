20280adecea81c176b168bba34c59d7ac0f37ff1
Olly Swanson <olly.swanson95@gmail.com>
2022-12-03 11:36:45 +0000

    Update Day 03 to use masks

--- src/day03.rs
+++ src/day03.rs
@@ -1,92 +1,72 @@
-pub fn run(input: &str) -> (usize, usize) {
-    let stats = process_input(input);
+use itertools::Itertools;
 
-    (part_1(&stats), part_2(&stats))
+pub fn run(input: &str) -> (u32, u32) {
+    let rucksacks = process_input(input);
+
+    (part_1(&rucksacks), part_2(&rucksacks))
 }
 
-fn process_input(input: &str) -> Vec<BagStats> {
-    input.lines().map(BagStats::process_line).collect()
+fn process_input(input: &str) -> Vec<Rucksack> {
+    input.lines().map(Rucksack::from_line).collect()
 }
 
-const fn letter_to_priority(byte: u8) -> u8 {
+const fn ascii_letter_to_mask(byte: u8) -> usize {
     if byte.is_ascii_lowercase() {
-        byte - b'a' + 1
+        1 << (byte - b'a')
     } else if byte.is_ascii_uppercase() {
-        byte - b'A' + 27
+        1 << (byte - b'A' + 26)
     } else {
         0
     }
 }
 
-// Use 64 for the sake of alignment
-#[derive(Debug)]
-struct BagStats([Stat; 64]);
+const fn mask_to_priority(mask: usize) -> u32 {
+    64 - usize::leading_zeros(mask)
+}
 
-impl BagStats {
-    // Bins fall into the range 1..=52 as specified by `letter_to_priority`
-    const PRIORITY_UPPER: usize = 52;
+struct Rucksack {
+    left: usize,
+    right: usize,
+}
 
-    fn process_line(line: &str) -> Self {
-        let mut stats = Self::default();
+impl Rucksack {
+    fn from_line(line: &str) -> Self {
         let bytes = line.as_bytes();
         let (left, right) = bytes.split_at(bytes.len() / 2);
 
-        for &byte in left {
-            let priority = letter_to_priority(byte) as usize;
-            let stat = &mut stats.0[priority];
-            stat.occurrences += 1;
-            stat.in_left = true;
-        }
+        let mask_fold = |acc: usize, &byte: &u8| acc | ascii_letter_to_mask(byte);
 
-        for &byte in right {
-            let priority = letter_to_priority(byte) as usize;
-            let stat = &mut stats.0[priority];
-            stat.occurrences += 1;
-            stat.in_right = true;
-        }
+        let left = left.iter().fold(0, mask_fold);
+        let right = right.iter().fold(0, mask_fold);
 
-        stats
+        Self { left, right }
+    }
+
+    fn intersect(&self) -> usize {
+        self.left & self.right
+    }
+
+    fn union(&self) -> usize {
+        self.left | self.right
     }
 }
 
-impl Default for BagStats {
-    fn default() -> Self {
-        Self([Stat::default(); 64])
-    }
+fn part_1(rucksacks: &[Rucksack]) -> u32 {
+    rucksacks
+        .iter()
+        .map(Rucksack::intersect)
+        .map(mask_to_priority)
+        .sum()
 }
 
-#[derive(Default, Clone, Copy, Debug)]
-struct Stat {
-    occurrences: u16,
-    in_left: bool,
-    in_right: bool,
-}
-
-fn part_1(stats: &[BagStats]) -> usize {
-    let priority_in_left_and_right = |stats: &BagStats| {
-        stats
-            .0
-            .iter()
-            .take(BagStats::PRIORITY_UPPER)
-            .position(|stat| stat.in_left && stat.in_right)
-    };
-
-    stats.iter().filter_map(priority_in_left_and_right).sum()
-}
-
-fn part_2(stats: &[BagStats]) -> usize {
-    assert!(stats.len() % 3 == 0);
-
-    (0..stats.len())
-        .step_by(3)
-        .map(|i| (&stats[i], &stats[i + 1], &stats[i + 2]))
-        .filter_map(|chunk| {
-            (0..BagStats::PRIORITY_UPPER + 1).position(|i| {
-                chunk.0 .0[i].occurrences > 0
-                    && chunk.1 .0[i].occurrences > 0
-                    && chunk.2 .0[i].occurrences > 0
-            })
-        })
+fn part_2(rucksacks: &[Rucksack]) -> u32 {
+    rucksacks
+        .iter()
+        .map(Rucksack::union)
+        .chunks(3)
+        .into_iter()
+        .map(|chunk| chunk.fold(usize::MAX, |acc, mask| mask & acc))
+        .map(mask_to_priority)
         .sum()
 }
 
@@ -103,16 +83,24 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw";
 
     #[test]
-    fn part_1_works() {
-        let stats = process_input(TEST_INPUT);
+    fn ascii_letter_to_mask_works() {
+        assert_eq!(1 << 0, ascii_letter_to_mask(b'a'));
+        assert_eq!(1 << 25, ascii_letter_to_mask(b'z'));
+        assert_eq!(1 << 26, ascii_letter_to_mask(b'A'));
+        assert_eq!(1 << 51, ascii_letter_to_mask(b'Z'));
+    }
 
-        assert_eq!(157, part_1(&stats));
+    #[test]
+    fn part_1_works() {
+        let rucksacks = process_input(TEST_INPUT);
+
+        assert_eq!(157, part_1(&rucksacks));
     }
 
     #[test]
     fn part_2_works() {
-        let stats = process_input(TEST_INPUT);
+        let rucksacks = process_input(TEST_INPUT);
 
-        assert_eq!(70, part_2(&stats));
+        assert_eq!(70, part_2(&rucksacks));
     }
 }

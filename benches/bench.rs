e09e6acecbc7dc9da78fa06d395ad3ff2c955b8b
Olly Swanson <olly.swanson95@gmail.com>
2022-12-05 15:11:06 +0000

    Create benchmark macro

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,39 +1,18 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-pub fn day01(c: &mut Criterion) {
-    use aoc2022::day01::run;
-    let input = include_str!("../inputs/day01.txt");
+macro_rules! benches {
+    ($($day:tt),*) => {
+        $(pub fn $day(c: &mut Criterion) {
+            use aoc2022::$day::run;
 
-    c.bench_function("day01", |b| b.iter(|| run(black_box(input))));
+            let input = include_str!(std::concat!("../inputs/", std::stringify!($day), ".txt"));
+
+            c.bench_function(std::stringify!($day), |b| b.iter(|| run(black_box(input))));
+        })*
+
+        criterion_group!(benches, $($day),*);
+    };
 }
 
-pub fn day02(c: &mut Criterion) {
-    use aoc2022::day02::run;
-    let input = include_str!("../inputs/day02.txt");
-
-    c.bench_function("day02", |b| b.iter(|| run(black_box(input))));
-}
-
-pub fn day03(c: &mut Criterion) {
-    use aoc2022::day03::run;
-    let input = include_str!("../inputs/day03.txt");
-
-    c.bench_function("day03", |b| b.iter(|| run(black_box(input))));
-}
-
-pub fn day04(c: &mut Criterion) {
-    use aoc2022::day04::run;
-    let input = include_str!("../inputs/day04.txt");
-
-    c.bench_function("day04", |b| b.iter(|| run(black_box(input))));
-}
-
-pub fn day05(c: &mut Criterion) {
-    use aoc2022::day05::run;
-    let input = include_str!("../inputs/day05.txt");
-
-    c.bench_function("day05", |b| b.iter(|| run(black_box(input))));
-}
-
-criterion_group!(benches, day01, day02, day03, day04, day05);
+benches!(day01, day02, day03, day04, day05);
 criterion_main!(benches);
